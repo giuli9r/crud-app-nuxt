af6d5f1bf3a38bb3e49d98d1539fac6b738a4489
giuli9r <giuli.rossetti9@gmail.com>
2026-09-11 10:38:46 -0300

    feat: add default layout and employee directory page with filtering and management capabilities

--- nuxt-app/layouts/default.vue
+++ nuxt-app/layouts/default.vue
@@ -1,22 +1,27 @@
 <template>
     <header class="shadow-sm bg-white">
         <nav class="container mx-auto p-4 flex justify-between items-center">
-            <ul  class="flex">
-                <li class="p-2"><NuxtLink to="/">Home</NuxtLink></li>
-                <li class="p-2"><NuxtLink to="/about">About</NuxtLink></li>
-                <li class="p-2"><NuxtLink to="/employee/employee">Employees</NuxtLink></li>
+            <ul class="flex">
+                <li class="p-2">
+                    <NuxtLink to="/">Home</NuxtLink>
+                </li>
+                <li class="p-2">
+                    <NuxtLink to="/about">About</NuxtLink>
+                </li>
+                <li class="p-2">
+                    <NuxtLink to="/employee/employees">Employees</NuxtLink>
+                </li>
             </ul>
         </nav>
     </header>
     <div class="container mx-auto p-4">
-        <slot/>
+        <slot />
     </div>
 </template>
 
 <style scoped>
-    .router-link-active {
-        font-weight: bold;
-        color: #3c834e;
-    }
-    
+.router-link-active {
+    font-weight: bold;
+    color: #3c834e;
+}
 </style>
